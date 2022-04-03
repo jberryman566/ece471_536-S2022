82ed45971209e1f082661928b4923f2f9947d66c
Josh Berryman <jberryman566@gmail.com>
2022-04-03 13:51:15 -0700

    Added find duck method

--- duck-hunt/solution.py
+++ duck-hunt/solution.py
@@ -1,4 +1,5 @@
 import time
+import cv2
 
 """
 Replace following with your own algorithm logic
@@ -31,6 +32,34 @@ def GetLocation(move_type, env, current_frame):
         Bottom right = (W, H) 
         """
         coordinate = env.action_space_abs.sample()
+
+        duck_image = cv2.imread("duck_images/green_duck.png")
+
+        ## Call find duck to get coords
+        coordinate = findDuck(current_frame, duck_image, 0.8)
     
     return [{'coordinate' : coordinate, 'move_type' : move_type}]
+
+## Function for searching for ducks using template matching
+def findDuck(game_screen, duck_image, threshold):
+
+    ## Get grayscale of game frame
+    grey_game_screen = cv2.cvtColor(game_screen, cv2.COLOR_BGR2GRAY)
+
+    ## Get grayscale of duck_image
+    grey_duck = cv2.cvtColor(duck_image, cv2.COLOR_BGR2GRAY)
+
+    ## Get dimensions of duck
+    w, h = grey_duck.shape[::-1]
+
+    ## Call match template to find a duck
+    res = cv2.matchTemplate(grey_game_screen, grey_duck, cv2.TM_CCOEFF_NORMED)
+    
+    loc = np.where(res >= threshold)
+
+    for pt in zip(*loc[::-1]):
+        cv2.rectangle(game_screen, pt, (pt[0]+w, pt[1]+h), (0,0,255), 2)
+
+    ## return coordinates of first found duck
+    return loc[0][0], loc[0][1]
 
